1fcda139a5b8913a384fe06530489e3cad4b8316
Hukum1020 <nikitasicily22@gmail.com>
2025-03-18 23:38:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,43 +48,30 @@ SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 if not SMTP_USER or not SMTP_PASSWORD:
     raise ValueError("❌ Ошибка: SMTP_USER или SMTP_PASSWORD не найдены!")
 
-def send_email(email, name, qr_filename, language):
+def send_email(email, qr_filename, language):
     try:
         msg = EmailMessage()
         msg["From"] = SMTP_USER
         msg["To"] = email
         msg["Subject"] = "Ваш QR-код" if language == "ru" else "QR-код билеті"
-
-        if language == "ru":
-            body = f"""Спасибо за регистрацию на BI Ecosystem!  
-
-Это ваш входной билет, пожалуйста, не удаляйте это письмо. QR-код нужно предъявить на входе для участия в розыгрыше ценных призов!  
-
-Ждём вас 5 апреля в 9:30 по адресу:  
-г. Алматы, проспект Аль-Фараби, 30, Almaty Teatre"""
-        else:  # "kz"
-            body = """BI Ecosystem жүйесіне тіркелгеніңізге рахмет! 
-
-Бұл сіздің кіруге арналған билетіңіз, өтініш осы хатты өшірмеңіз. Ұтыс ойындарында қатысу үшін осы QR кодты кіру есігі алдында көрсету қажет.
-
-Сізді 5 сәуір күні сағат 09:30 Алматы қаласы, Әл-Фараби даңғылы, 30 мекен жайы бойынша күтеміз."""
-
-        msg.set_content(body)
-
+        
+        template_filename = f"Ala{language}.html"
+        
+        # Прикрепляем QR-код
         with open(qr_filename, "rb") as qr_file:
-            msg.add_attachment(
-                qr_file.read(),
-                maintype="image",
-                subtype="png",
-                filename="qrcode.png"
-            )
-
+            msg.add_attachment(qr_file.read(), maintype="image", subtype="png", filename="qrcode.png")
+        
+        # Прикрепляем соответствующий HTML шаблон
+        if os.path.exists(template_filename):
+            with open(template_filename, "rb") as template_file:
+                msg.add_attachment(template_file.read(), maintype="text", subtype="html", filename=template_filename)
+        
         context = ssl.create_default_context()
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls(context=context)
             server.login(SMTP_USER, SMTP_PASSWORD)
             server.send_message(msg)
-
+        
         print(f"[OK] Письмо отправлено на {email}")
         return True
     except Exception as e:
